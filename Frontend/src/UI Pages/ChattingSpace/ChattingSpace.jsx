19d76e000cc2cd0cda7efb104f7184abe09efee6
SandeepSaran27 <1casandeepsaran@gmail.com>
2026-06-10 17:00:01 +0530

    Update ChattingSpace.jsx

--- Frontend/src/UI Pages/ChattingSpace/ChattingSpace.jsx
+++ Frontend/src/UI Pages/ChattingSpace/ChattingSpace.jsx
@@ -76,6 +76,7 @@ function ChattingSpace() {
                 }
             );
         const data = await res.json();
+        console.log("res", res);
         console.log("Data object at send msg", data);
         const userId = data.user_id;
         socket.emit("sendmessage", {
